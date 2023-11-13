7f7e431c24eac3d91cb3ac9cd915323022857e83
poomrapee bunjong <poomrapee.bun@gmail.com>
2023-11-13 13:56:34 +0700

    ใส่หน้า register
    -ดึงแผนที่จาก api google
    -ปรับปรุงพื้นหลัง

---
+++ Website/inputpage.js
@@ -0,0 +1,7 @@
+var map;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 13.736694 , lng:100.531658},
+        zoom: 14
+        });
+    }
