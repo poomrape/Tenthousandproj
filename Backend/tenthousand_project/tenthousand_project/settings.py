8a000aca942a4b7e80dfa83cfced450773df3ac6
KengKaK <suppadej3@gmail.com>
2023-11-16 16:12:04 +0700

    .

--- Backend/tenthousand_project/tenthousand_project/settings.py
+++ Backend/tenthousand_project/tenthousand_project/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         "ENGINE": "mysql.connector.django",
         "NAME": "10thousandproject_db",
         "USER": "kengkak",
-        "PASSWORD": "hgo092262",
+        "PASSWORD": "",
         "HOST": "127.0.0.1",
         "PORT": "3306"
     }
