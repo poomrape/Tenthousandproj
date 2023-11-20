39b3ab3c3cd43de3a483486f710c82547ca09ae2
3lor <puuu2312@gmail.com>
2023-11-20 19:24:32 +0700

    Register layout

---
+++ Website/Registerpage.js
@@ -0,0 +1,9 @@
+function goto_loginpage (){
+    window.location.href = "Loginpage.html";
+}
+function get_value(){
+    var username= document.getElementById("Usernamebox").value;
+    var password= document.getElementById("Passwordbox").value;
+    alert('username='+username+' password='+password)
+    window.location.href = "Loginpage.html";
+}
