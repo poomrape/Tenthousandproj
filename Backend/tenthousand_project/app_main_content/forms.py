ce39c5393c95ef9e4517efeafb19adf65143df16
KengKaK <suppadej3@gmail.com>
2023-11-21 21:41:35 +0700

    inputpage

--- Backend/tenthousand_project/app_main_content/forms.py
+++ Backend/tenthousand_project/app_main_content/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class LocationForm(forms.Form):
-    location = forms.CharField(max_length=255, required=False)
+    location = forms.CharField(max_length=255, required=False, widget=forms.TextInput(attrs={'placeholder': 'Enter location'}))
 
 class RadiusForm(forms.Form):
-    radius = forms.CharField(max_length=255, required=False)
+    radius = forms.CharField(max_length=255, required=False, widget=forms.TextInput(attrs={'placeholder': 'Radius'}))
